559942313fcf49649e32f1af4c3e72f59d788c97
BentouDev <bentkowski.jakub@gmail.com>
2020-09-06 11:50:31 +0200

    Special attack prototype

---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    public float Lifetime = 2.0f;
+    public float Speed = 1.0f;
+
+    private float SpawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 2.0f, Color.red);
+        transform.position += transform.forward.normalized * Speed * Time.deltaTime;
+
+        if (Lifetime < Time.time - SpawnTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var health = other.GetComponent<Health>();
+        if (health)
+        {
+            health.TakeDamage(200);
+        }
+    }
+}
---
+++ Assets/Scripts/SpecialAttackHandler.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialAttackHandler : MonoBehaviour
+{
+    public GameObject BombPrefab;
+    public List<Transform> Bombs = new List<Transform>();
+    public float BombScale;
+    public Vector3 BombOffset;
+
+    public bool DeactivateInsteadOfScale;
+
+    private bool IsDuringSpecial = false;
+
+    public void DetachBomb(string dummy)
+    {
+        //Debug.Assert(!IsDuringSpecial);
+        //if (IsDuringSpecial)
+        //    return;
+
+        IsDuringSpecial = true;
+
+        foreach (var bomb in Bombs)
+        {
+            var newBomb = Instantiate(BombPrefab, null);
+            newBomb.transform.position = bomb.transform.position + BombOffset;
+            newBomb.transform.localScale = new Vector3(BombScale, BombScale, BombScale);
+            newBomb.transform.forward = transform.forward;
+            //newBomb.transform.rotation = bomb.transform.rotation;
+
+            if (DeactivateInsteadOfScale)
+                bomb.gameObject.SetActive(false);
+            else
+                bomb.transform.localScale = Vector3.zero;
+        }
+    }
+
+    public void SpawnBomb(string dummy)
+    {
+        //Debug.Assert(IsDuringSpecial);
+        //if (!IsDuringSpecial)
+        //    return;
+
+        StartCoroutine(CoSpawnBomb());
+    }
+
+    IEnumerator CoSpawnBomb()
+    {
+        const float Duration = 0.5f;
+        float elapsed = 0.0f;
+
+        if (DeactivateInsteadOfScale)
+            foreach (var bomb in Bombs)
+            {
+                bomb.gameObject.SetActive(true);
+            }
+        else
+        {
+            while (elapsed < Duration)
+            {
+                elapsed += Time.deltaTime;
+
+                float coefficient = elapsed / Duration;
+
+                foreach (var bomb in Bombs)
+                {
+                    bomb.localScale = new Vector3(coefficient, coefficient, coefficient);
+                }
+
+                yield return null;
+            }
+
+            foreach (var bomb in Bombs)
+            {
+                bomb.localScale = new Vector3(BombScale, BombScale, BombScale);
+            }
+        }
+
+        IsDuringSpecial = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
