ddadebdf3605b7b4da0f8b1e80bd6af5ecf80380
BentouDev <bentkowski.jakub@gmail.com>
2020-09-06 02:34:25 +0200

    Limit player movement, increase turn time limit

---
+++ Assets/Scripts/WorldPosLimiter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldPosLimiter : MonoBehaviour
+{
+    public float MaxWorldPos = 12;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > MaxWorldPos || transform.position.x < -MaxWorldPos)
+        {
+            var pos = transform.position;
+            pos.x = Mathf.Sign(transform.position.x) * MaxWorldPos;
+
+            transform.position = pos;
+        }
+    }
+}
